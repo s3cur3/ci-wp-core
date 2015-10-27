1c943f6d5037b0cbae916710d8d7edac9e8444c7
Tyler Young <tyler@x-plane.com>
2015-10-27 17:24:07 -0500

    fix (prettyPhoto): Galleries now properly pop up prettyPhoto overlays

--- theme/scripts.php
+++ theme/scripts.php
@@ -18,7 +18,7 @@ function roots_scripts()
         wp_enqueue_script('comment-reply');
     }
 
-    wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', array('jquery'), '379e093478d340ae48d060be265d0c31', true);
+    wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', array('jquery'), '83f755daf5654f8ef71160dcdb858c18', true);
     wp_enqueue_script('jquery');
     wp_enqueue_script('roots_scripts');
 
